3503ef94415952f20e578fa5b4c20dc7de0e8c6f
Daniel Weber <d.weber16@zohomail.com>
2022-07-02 20:17:28 -0500

    finished game

--- guess_number.py
+++ guess_number.py
@@ -4,3 +4,15 @@ secret_number = random.randint(1, 100)
 
 print("Welcome to my guessing game!")
 
+guess = int(input("Please make a guess! ", ))
+
+while guess != secret_number:
+    if guess < secret_number:
+        print("Too low.")
+        guess = int(input("Try again: "))
+    elif guess > secret_number:
+        print("Too high.")
+        guess = int(input("Try again: "))
+
+if guess == secret_number:
+    print("You win!!")
